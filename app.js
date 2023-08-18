456859f55a28e7f6d9012d627b31b78206d39c4a
Zaid-Dildar <142285065+Zaid-Dildar@users.noreply.github.com>
2023-08-18 09:54:51 +0500

    handling returnTo

--- app.js
+++ app.js
@@ -128,8 +128,8 @@ passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
 app.use((req, res, next) => {
-
-    if (!['/login'].includes(req.originalUrl)) {
+    req.session.returnTo = '/'
+    if (!['/login', '/'].includes(req.originalUrl)) {
         req.session.returnTo = req.originalUrl
     }
     res.locals.currentUser = req.user
